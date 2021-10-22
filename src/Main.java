587968dbd708b9177e0571d36316b55ed143a5bc
demmysabel <ekprokhorenko@gmail.com>
2021-10-22 11:41:30 +0300

    money transfer

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int transferMoney = 500_000_000;
+        int total = balance + transferMoney;
+        System.out.println(total);
+    }
+}
